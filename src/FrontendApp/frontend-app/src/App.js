fde3e9a2012b2a3aa2d1118bc81eda313b749604
Damian N <dnaprawa@gmail.com>
2019-11-24 09:17:36 +0100

    Getting host IP address

--- src/FrontendApp/frontend-app/src/App.js
+++ src/FrontendApp/frontend-app/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ip from 'ip';
 
 function App() {
+
+  const [message, setIpAddress] = useState();
+
+  useEffect(() => {
+    let hostIP = ip.address();
+    setIpAddress(hostIP);
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React <code>src/App.js</code> and save to reload.
+          Hello from: {message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
     </div>
   );
